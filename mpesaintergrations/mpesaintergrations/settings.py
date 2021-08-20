d95e9d893a42732b8bbfbc5afae107e3ca5daf7f
Maurice Omondi Okoth <momondi547@gmail.com>
2021-08-20 12:34:10 +0300

    updated settings.py

--- mpesaintergrations/mpesaintergrations/settings.py
+++ mpesaintergrations/mpesaintergrations/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&b@p!30i)q)l56n_sdgp&vh4$gzn$8^p@nc&9l^d_de=8jq2#i'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
